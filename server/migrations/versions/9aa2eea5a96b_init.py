70e32bd38b0290ceb2f3d1193dd94617a185ff3f
ngigi1234 <francis.ngigi1@student.moringaschool.com>
2024-04-14 03:00:02 +0300

    Done

---
+++ server/migrations/versions/9aa2eea5a96b_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 9aa2eea5a96b
+Revises: 1fbfa43fdc29
+Create Date: 2024-02-02 09:23:35.656605
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9aa2eea5a96b'
+down_revision = '1fbfa43fdc29'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
